8250e86e368acf657f8d76545237184fd947d6a6
Ethen Pociask <ethen@eigenlabs.org>
2024-12-16 22:15:04 +0700

    chore: Bump WASM module rot

--- src/createRollupPrepareDeploymentParamsConfigDefaults.ts
+++ src/createRollupPrepareDeploymentParamsConfigDefaults.ts
@@ -1,7 +1,7 @@
 import { parseEther, zeroAddress } from 'viem';
 
 export const wasmModuleRoot: `0x${string}` =
-  '0x59e9a1ea4ae5fa88d6cee2f257bd7b717b0b5b51c2b96c4289ce371843c3e914';
+  '0x8729b9f8bca48ca91a8392160e5d5b319a63a267efe33fd145493cd0ca6c6b7b';
 
 export const defaults = {
   extraChallengeTimeBlocks: BigInt(0),
