61eedbeba16fc01d180e8cb6400d71094809111c
Ethen Pociask <ethen@eigenlabs.org>
2024-12-18 03:21:56 +0700

    chore: Bump to using consensus-v32-eigenda module root

--- src/createRollupPrepareDeploymentParamsConfigDefaults.ts
+++ src/createRollupPrepareDeploymentParamsConfigDefaults.ts
@@ -1,7 +1,7 @@
 import { parseEther, zeroAddress } from 'viem';
 
 export const wasmModuleRoot: `0x${string}` =
-  '0x8729b9f8bca48ca91a8392160e5d5b319a63a267efe33fd145493cd0ca6c6b7b';
+  '0x951009942c00b5bd0abec233174fe33fadf7cd5013d17b042f9b28b3b00b469c';
 
 export const defaults = {
   extraChallengeTimeBlocks: BigInt(0),
