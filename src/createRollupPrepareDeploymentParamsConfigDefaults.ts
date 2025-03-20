da7db6ec408ad27a38512c575672aac05d02b0a4
ethenotethan <ethen@eigenlabs.org>
2025-03-20 21:32:08 +0900

    chore: Bump default consensus root to eigenda-v32.1 (#11)

--- src/createRollupPrepareDeploymentParamsConfigDefaults.ts
+++ src/createRollupPrepareDeploymentParamsConfigDefaults.ts
@@ -1,8 +1,8 @@
 import { parseEther, zeroAddress } from 'viem';
 
-// https://github.com/Layr-Labs/nitro/releases/tag/consensus-eigenda-v32
+// https://github.com/Layr-Labs/nitro/releases/tag/consensus-eigenda-v32.1
 export const wasmModuleRoot: `0x${string}` =
-  '0x951009942c00b5bd0abec233174fe33fadf7cd5013d17b042f9b28b3b00b469c';
+  '0x04a297cdd13254c4c6c26388915d416286daf22f3a20e3ebee10400a3129dd17';
 
 export const defaults = {
   extraChallengeTimeBlocks: BigInt(0),
